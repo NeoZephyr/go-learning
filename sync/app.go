22d9269a76f16ccd6e7f0de4f88af8ab7a2f73a6
meilb <meimlb@163.com>
2021-02-06 13:57:14 +0800

    update

--- sync/app.go
+++ sync/app.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"sync"
 	"sync/atomic"
@@ -28,46 +31,69 @@ func (c *counter) incr(i uint) uint {
 	return c.number
 }
 
+type Buffer interface {
+	Delimiter() byte
+
+	Write(content string) (err error)
+
+	Read() (content string, err error)
+
+	Free()
+}
+
+type delimiterBuffer struct {
+	buf       bytes.Buffer
+	delimiter byte
+}
+
+var bufPool sync.Pool
+var delimiter = byte('\n')
+
+func (b *delimiterBuffer) Delimiter() byte {
+	return b.delimiter
+}
+
+func (b *delimiterBuffer) Write(content string) (err error) {
+	if _, err := b.buf.WriteString(content); err != nil {
+		return err
+	}
+
+	return b.buf.WriteByte(b.delimiter)
+}
+
+func (b *delimiterBuffer) Read() (content string, err error) {
+	return b.buf.ReadString(b.delimiter)
+}
+
+func (b *delimiterBuffer) Free() {
+	bufPool.Put(b)
+}
+
+func getBuffer() Buffer {
+	return bufPool.Get().(Buffer)
+}
+
+func init() {
+	bufPool = sync.Pool{
+		New: func() interface{} {
+			return &delimiterBuffer{delimiter: delimiter}
+		},
+	}
+}
+
 func main() {
 	// mutexCountApp()
 	// sendAndRecvApp()
 	// chanCountApp()
 	// wgCountApp()
-	contextCountApp()
+	// contextCountApp()
+
+	// syncPoolApp()
 }
 
-func send(lock *sync.Mutex, sendCond *sync.Cond, recvCond *sync.Cond, mailbox *uint8, id int, index int) {
-	lock.Lock()
-
-	for *mailbox == 1 {
-		sendCond.Wait()
-	}
-
-	log.Printf("sender%d [%d]: empty", id, index)
-
-	*mailbox = 1
-
-	log.Printf("sender%d [%d]: the letter has been sent", id, index)
-
-	lock.Unlock()
-	recvCond.Broadcast()
-}
-
-func recv(lock *sync.Mutex, sendCond *sync.Cond, recvCond *sync.Cond, mailbox *uint8, id int, index int) {
-	lock.Lock()
-
-	for *mailbox == 0 {
-		recvCond.Wait()
-	}
-
-	log.Printf("receiver%d [%d]: full", id, index)
-
-	*mailbox = 0
-
-	log.Printf("receiver%d [%d]: the letter has been received", id, index)
-
-	lock.Unlock()
-	sendCond.Broadcast()
+func mutexCountApp() {
+	c := counter{}
+	count(&c)
 }
 
 func sendAndRecvApp() {
@@ -112,52 +138,6 @@ func sendAndRecvApp() {
 			recv(&lock, sendCond, recvCond, &mailbox, id, i)
 		}
 	}(2, max/2)
-
-	<-sign
-	<-sign
-	<-sign
-}
-
-func mutexCountApp() {
-	c := counter{}
-	count(&c)
-}
-
-func count(c *counter) {
-	sign := make(chan struct{}, 3)
-
-	go func() {
-		defer func() {
-			sign <- struct{}{}
-		}()
-
-		for i := 0; i < 5; i++ {
-			time.Sleep(time.Millisecond * 500)
-			c.incr(1)
-		}
-	}()
-
-	go func() {
-		defer func() {
-			sign <- struct{}{}
-		}()
-
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Millisecond * 200)
-			log.Printf("Go 1 read number in counter: [%d - %d]", i, c.num())
-		}
-	}()
-
-	go func() {
-		defer func() {
-			sign <- struct{}{}
-		}()
-
-		for i := 0; i < 10; i++ {
-			time.Sleep(time.Millisecond * 300)
-			log.Printf("Go 2 read number in counter: [%d - %d]", i, c.num())
-		}
-	}()
 
 	<-sign
 	<-sign
@@ -209,6 +189,104 @@ func contextCountApp() {
 	<-ctx.Done()
 
 	log.Printf("count: %d\n", count)
+}
+
+func syncPoolApp() {
+	buf := getBuffer()
+	defer buf.Free()
+
+	buf.Write("A Pool is a set of temporary objects")
+	buf.Write("A Pool is safe for use by multiple goroutines simultaneously")
+	buf.Write("A Pool must not be copied after first use")
+
+	for {
+		block, err := buf.Read()
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			panic(fmt.Errorf("unexpected error: %+v", err))
+		}
+
+		fmt.Print(block)
+	}
+}
+
+func send(lock *sync.Mutex, sendCond *sync.Cond, recvCond *sync.Cond, mailbox *uint8, id int, index int) {
+	lock.Lock()
+
+	for *mailbox == 1 {
+		sendCond.Wait()
+	}
+
+	log.Printf("sender%d [%d]: empty", id, index)
+
+	*mailbox = 1
+
+	log.Printf("sender%d [%d]: the letter has been sent", id, index)
+
+	lock.Unlock()
+	recvCond.Broadcast()
+}
+
+func recv(lock *sync.Mutex, sendCond *sync.Cond, recvCond *sync.Cond, mailbox *uint8, id int, index int) {
+	lock.Lock()
+
+	for *mailbox == 0 {
+		recvCond.Wait()
+	}
+
+	log.Printf("receiver%d [%d]: full", id, index)
+
+	*mailbox = 0
+
+	log.Printf("receiver%d [%d]: the letter has been received", id, index)
+
+	lock.Unlock()
+	sendCond.Broadcast()
+}
+
+func count(c *counter) {
+	sign := make(chan struct{}, 3)
+
+	go func() {
+		defer func() {
+			sign <- struct{}{}
+		}()
+
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Millisecond * 500)
+			c.incr(1)
+		}
+	}()
+
+	go func() {
+		defer func() {
+			sign <- struct{}{}
+		}()
+
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 200)
+			log.Printf("Go 1 read number in counter: [%d - %d]", i, c.num())
+		}
+	}()
+
+	go func() {
+		defer func() {
+			sign <- struct{}{}
+		}()
+
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 300)
+			log.Printf("Go 2 read number in counter: [%d - %d]", i, c.num())
+		}
+	}()
+
+	<-sign
+	<-sign
+	<-sign
 }
 
 func incr(pCount *int32, id int32, max int32, deferFunc func()) {

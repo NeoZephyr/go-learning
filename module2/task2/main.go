7f8cd5a49825ba45981cef3b33a234f98b86dec5
meilb <meimlb@163.com>
2022-01-10 00:02:38 +0800

    fix: update task2

--- module2/task2/main.go
+++ module2/task2/main.go
@@ -6,10 +6,11 @@ import (
 	"time"
 )
 
+var stop = false
+
 func produce(no int, wg *sync.WaitGroup, c chan <- string) {
 	counter := 0
-	wg.Add(1)
-	for {
+	for !stop {
 		time.Sleep(time.Second)
 		c <- fmt.Sprintf("Producer %d ---> %d", no, counter)
 		counter++
@@ -18,24 +19,36 @@ func produce(no int, wg *sync.WaitGroup, c chan <- string) {
 }
 
 func consume(no int, wg *sync.WaitGroup, c <- chan string) {
-	wg.Add(1)
 	for v := range c {
-		fmt.Printf("Consumer %d consume message: %s", no, v)
+		time.Sleep(time.Second)
+		fmt.Printf("Consumer %d consume message: %s\n", no, v)
 	}
 	wg.Done()
 }
 
 func main() {
-	var c chan string
+	c := make(chan string, 10)
 	pwg := &sync.WaitGroup{}
 	cwg := &sync.WaitGroup{}
 
 	for i := 0; i < 3; i++ {
+		pwg.Add(1)
 		go produce(i, pwg, c)
 	}
 
-	go consume(0, cwg, c)
+	for i := 0; i < 2; i++ {
+		cwg.Add(1)
+		go consume(i, cwg, c)
+	}
+
+	go func() {
+		time.AfterFunc(time.Second * 10, func() {
+			stop = true
+			fmt.Println("stop produce!!!")
+		})
+	}()
 
 	pwg.Wait()
+	close(c)
 	cwg.Wait()
 }
